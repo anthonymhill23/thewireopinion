d8a29f5d31da49da63d888a030de446291ab3fbb
Anthony Hill <anthonymhill23@gmail.com>
2021-10-07 02:17:24 -0400

    added jsfile

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 let userName = prompt('Please enter your name!');
 console.log(userName)
+let userAnswer = ('Have you seen the wire?');
+console.log('the answer to the game is:'+userAnswer);
+
+if(userAnswer == 'yes'{
+    let url = 'https://gfycat.com/fancyembarrassedjaguar';
+    
+}
