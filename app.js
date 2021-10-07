1015c298bef29f4080779efc90da5fc91b08d1d4
Anthony Hill <anthonymhill23@gmail.com>
2021-10-07 02:23:18 -0400

    added jsfile two

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ console.log(userName)
 let userAnswer = ('Have you seen the wire?');
 console.log('the answer to the game is:'+userAnswer);
 
-if(userAnswer == 'yes'){
-    let url = 'https://gfycat.com/fancyembarrassedjaguar';
+
     
-}
+    
