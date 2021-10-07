b69bc0fd45b83e682e8ac1013038b4b41b6a2bcf
Anthony Hill <anthonymhill23@gmail.com>
2021-10-07 16:25:12 -0400

    deleted app2,modified my index file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let userName = prompt('Please enter your name!');
 console.log(userName)
 let userAnswer = prompt('Have you seen the wire?');
+let url = ''
 if (userAnswer == 'yes'){
-    let url = 'https://c.tenor.com/PpDIJg9rTjUAAAAC/wire-the-bunk-oh-indeed.gif';
-    document.write( '<img id="indeed"  src= "' + url +'">')
+     url = 'https://c.tenor.com/PpDIJg9rTjUAAAAC/wire-the-bunk-oh-indeed.gif';
+   
+}else if (userAnswer == 'no'){
+
+     url = 'https://c.tenor.com/b0kAsOeq9mcAAAAC/stringer-bell-atef.gif'
+    
 }
-
-if (userAnswer == 'no'){
-
-let url = 'https://c.tenor.com/b0kAsOeq9mcAAAAC/stringer-bell-atef.gif'
 document.write( '<img id="walkoff"  src= "' + url +'">')
-}
 
 
 
